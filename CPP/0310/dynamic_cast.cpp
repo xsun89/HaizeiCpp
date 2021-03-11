56fbcdf65cd2f6080ec010c1f5c50d1e4c7c4be4
xsun <sunxin89@gmail.com>
2021-03-11 08:48:35 -0800

    cpp

--- CPP/0310/dynamic_cast.cpp
+++ CPP/0310/dynamic_cast.cpp
@@ -28,19 +28,32 @@ public:
     virtual ~C(){}
 };
 
+void judge(A *p){
+    //dynamic_cast cast derived object to it's bass object
+    //if type matches, return it, if not match return NULL. rely on virtual
+    //table to determine.
+    if(dynamic_cast<B *>(p)){
+        cout << "this is B" << endl;
+    }else if(dynamic_cast<C *>(p)){
+        cout << "this is C" << endl;
+    }else{
+        cout << "NULL" << endl;
+    }
+    return;
+}
+
 int main()
 {
     srand(time(NULL));
     A* p;
+    B* b;
+    C* c;
     switch(rand() % 2){
         case 0: p = new B(); break;
         case 1: p = new C(); break;
     }
-    //dynamic_cast cast derived object to it's bass object
-    //if type matches, return it, if not match return NULL. rely on virtual
-    //table to determine.
-    cout << dynamic_cast<B *>(p) << endl;
-    cout << dynamic_cast<C *>(p) << endl;
+    judge(p);
 
+    ((void **)&b)[0] = ((void **)&c)[0];
     return 0;
 }
