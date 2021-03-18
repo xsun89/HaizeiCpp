da7d9694ff7e54c89078ed68d473571db553d27d
xsun <sunxin89@gmail.com>
2021-03-18 11:10:48 -0700

    ok

--- CPP/0313/is_prime.cpp
+++ CPP/0313/is_prime.cpp
@@ -19,7 +19,17 @@ struct is_prime{
     static constexpr char *r = (char *)(isPrime<n, n - 1>::r ? "No" : "Yes");
 };
 
+template <int n>
+struct isEven {
+    static constexpr int r = !(n % 2);
+};
+
+template <int n>
+struct is_even{
+    static constexpr char *r = (char *)(isEven<n>::r ? "Yes" : "No");
+};
 int main(){
     cout << is_prime<5>::r << endl;
+    cout << is_even<4>::r << endl;
     return 0;
 }
